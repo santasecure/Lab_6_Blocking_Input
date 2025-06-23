1d007e21c6990a30d2722759f865e5663d0af01c
Kristie Santa <k-santa@hotmail.com>
2025-06-22 22:16:44 -0400

    Updated with 'Do While' code

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,23 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-// Pseudocode: Prompt user for temperature in Fahrenheit, convert to Celsius, then display result.
 
+// Convert Fahrenheit to Celsius using a do-while loop to validate input
 import java.util.Scanner;
 
 public class TemperatureConverter {
     public static void main(String[] args) {
-        Scanner inputScanner = new Scanner(System.in);
+        // Pseudocode: Prompt user for Fahrenheit, validate input with do-while, convert to Celsius, display message
+        Scanner in = new Scanner(System.in);
+        boolean validInput = false;
+        double fahrenheit = 0.0;
 
-        // Prompt the user for Fahrenheit temperature
-        System.out.print("Enter temperature in Fahrenheit: ");
+        do {
+            System.out.print("Enter temperature in Fahrenheit: ");
+            if (in.hasNextDouble()) {
+                fahrenheit = in.nextDouble();
+                validInput = true;
+            } else {
+                String trash = in.next();
+                System.out.println("You must enter a valid number. You entered: \"" + trash + "\"");
+            }
+        } while (!validInput);
 
-        // Read user input
-        double fahrenheitTemp = inputScanner.nextDouble();
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        System.out.printf("%.1f°F is %.1f°C%n", fahrenheit, celsius);
 
-        // Convert to Celsius
-        double celsiusTemp = (fahrenheitTemp - 32) * 5 / 9;
-
-        // Output result
-        System.out.println("The equivalent temperature in Celsius is: " + celsiusTemp);
+        if (fahrenheit == 32.0) {
+            System.out.println("That's the freezing point of water.");
+        } else if (fahrenheit == 212.0) {
+            System.out.println("That's the boiling point of water.");
+        }
     }
 }
+
+
+
+
+
